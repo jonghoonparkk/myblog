15cf1aa18508d10efe3d4d2360d94a32b8b5e11e
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-10 16:19:26 +0900

    Update page.tsx

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -14,7 +14,7 @@ export default async function Page() {
   return (
     <>
       <div className={styles.background}>
-        <div className={`${styles.background} pt-12`}> {/* 여기에서 pt-12로 상단 패딩 추가 */}
+        <div className={`${styles.background} pt-12`}> 
           <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
             <div className="space-x-2 pb-8 pt-6 md:space-y-5">
               <h1 className="text-3xl font-black leading-9 tracking-tight text-gray-100 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
