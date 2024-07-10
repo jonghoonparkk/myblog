4f510ba9b2b92bd574dc87acf6591615226577be
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-10 13:39:16 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden h-6 text-2xl font-extrabold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
