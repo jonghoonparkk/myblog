b3bbd6e43bf99b671b661041db07faf60206643b
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-11 21:27:26 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import SearchButton from './SearchButton'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-10">
+    <header className="font-thin flex items-center justify-between py-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
