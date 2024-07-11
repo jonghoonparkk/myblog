42bcf142d6d961f236aa90e2f1aa4c98bd638495
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-11 23:53:25 +0900

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -81,7 +81,7 @@ export default function Home({ posts }) {
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="All posts"
           >
-            All Posts &rarr;
+            모든 글 보기 &rarr;
           </Link>
         </div>
       )}
