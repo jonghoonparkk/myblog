469de85bed1175a710dc3a4b5b462b5d3bb73d4d
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-11 21:29:35 +0900

    ㅇ

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,16 +1,14 @@
 // app/tags/page.tsx
-import tagData from 'app/tag-data.json'
-import { genPageMetadata } from 'app/seo'
-import ClientComponent from './ClientComponent' // 클라이언트 컴포넌트 임포트
+import tagData from 'app/tag-data.json';
+import { genPageMetadata } from 'app/seo';
+import ClientComponent from './ClientComponent'; // 클라이언트 컴포넌트 임포트
 
-export const metadata = genPageMetadata({ title: 'Tags', description: 'Things I blog about' })
+export const metadata = genPageMetadata({ title: 'Tags', description: 'Things I blog about' });
 
 export default async function Page() {
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
-  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
-  
-  return (
-    <ClientComponent sortedTags={sortedTags} tagCounts={tagCounts} />
-  )
+  const tagCounts = tagData as Record<string, number>;
+  const tagKeys = Object.keys(tagCounts);
+  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a]);
+
+  return <ClientComponent sortedTags={sortedTags} tagCounts={tagCounts} />;
 }
