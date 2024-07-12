e8a76da78a23b80f0189d963ba3f990e8090a243
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-12 23:33:35 +0900

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/module', title: 'Module' },
+  { href: '/events', title: 'Events' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/events', title: 'Events' },
   { href: '/about', title: 'About' },
-  { href: '/module', title: 'Module' },
 ]
 
 export default headerNavLinks
