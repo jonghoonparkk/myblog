5a117d16603e9f9c83df7213d4f755b927731fce
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-12 17:13:18 +0900

    Update ListLayoutWithTags.tsx

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -134,7 +134,7 @@ export default function ListLayoutWithTags({
                       </dl>
                       <div className="space-y-3">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h2 className="text-2xl font-extralight leading-8 tracking-tight">
                             <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
                               {title}
                             </Link>
