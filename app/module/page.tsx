fa0b69c4873fe22c28c988b0c549c5478fd0d9ea
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-12 21:22:34 +0900

    sd

--- app/module/page.tsx
+++ app/module/page.tsx
@@ -1,4 +1,4 @@
-import projectsData from '@/data/projectsData'
+import modulesData from '@/data/modulesData'
 import Card from '@/components/Card'
 import { genPageMetadata } from 'app/seo'
 
@@ -10,13 +10,13 @@ export default function Modules() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-2xl font-extralight leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Events
+            Module
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">모듈</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">총 5가지의 모듈 중 자신의 단계에 맞게 선택해서 하나씩 정복해보자! 👻</p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
+            {modulesData.map((d) => (
               <Card
                 key={d.title}
                 title={d.title}
---
+++ data/modulesData.ts
@@ -0,0 +1,42 @@
+interface Module {
+  title: string,
+  description: string,
+  href?: string,
+  imgSrc?: string,
+}
+
+const modulesData: Module[] = [
+  {
+    title: '🐍파이썬 모듈🐍',
+    description: `인공지능에 가장 많이 쓰이는 파이썬 언어를 배워봅시다.`,
+    imgSrc: '/static/images/module/ai/0.png',
+    href: 'https://hyai.vercel.app/blog/%ED%8C%8C%EC%9D%B4%EC%8D%AC%20%EB%AA%A8%EB%93%88',
+  },
+  {
+    title: '🔧C언어 모듈🔧',
+    description: `시스템 프로그래밍과 임베디드 시스템에서 널리 사용되는 C언어를 배워봅시다.`,
+    imgSrc: '/static/images/module/c/m3/1.png',
+    href: 'https://hyai.vercel.app/blog/C%EC%96%B8%EC%96%B4%20%EB%AA%A8%EB%93%88',
+  },
+  {
+    title: '🤖인공지능 모듈🤖',
+    description: `인공지능의 세계에 첫 발을 내딛어 봅시다. 인공지능(AI)은 데이터를 통해 학습하고, 예측하며, 인간과 비슷한 지능적인 작업을 수행하는 기술입니다.`,
+    imgSrc: '/static/images/module/ai/3.png',
+    href: 'https://hyai.vercel.app/blog/%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5%20%EB%AA%A8%EB%93%88',
+  },
+  {
+    title: '👻머신러닝 모듈👻',
+    description: `머신러닝 모듈을 통해 현대 데이터 과학의 핵심 기술인 머신러닝을 배워봅시다.`,
+    imgSrc: '/static/images/module/ai/5.png',
+    href: 'https://hyai.vercel.app/blog/%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D%20%EB%AA%A8%EB%93%88',
+  },
+  {
+    title: '🧠딥러닝 모듈🧠',
+    description: `딥러닝의 기본 개념과 원리를 배워봅시다. 딥러닝은 인공지능의 한 분야로, 인공 신경망을 기반으로 하는 알고리즘을 사용하여 데이터를 학습하고 예측하는 기술입니다.`,
+    imgSrc: '/static/images/module/ai/2.png',
+    href: 'https://hyai.vercel.app/blog/%EB%94%A5%EB%9F%AC%EB%8B%9D%20%EB%AA%A8%EB%93%88',
+  },
+  
+]
+
+export default modulesData
