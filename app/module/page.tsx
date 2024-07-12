695d64501ad53e2ee1e0a8fc25d2295fa9c1de76
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-12 21:25:21 +0900

    Update page.tsx

--- app/module/page.tsx
+++ app/module/page.tsx
@@ -12,7 +12,9 @@ export default function Modules() {
           <h1 className="text-2xl font-extralight leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Module
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">총 5가지의 모듈 중 자신의 단계에 맞게 선택해서 하나씩 정복해보자! 👻</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            총 5가지의 모듈 중 자신의 단계에 맞게 선택해서 하나씩 정복해보자! 👻
+          </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
