8fc26012250ad112c8179a6eca064b2aaa404af4
hpic-lab <hoon765@hanyang.ac.kr>
2024-07-12 15:44:53 +0900

    s

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -125,7 +125,7 @@ export default function ListLayout({
                   </dl>
                   <div className="space-y-3 xl:col-span-3">
                     <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                      <h3 className="text-2xl font-extralight leading-8 tracking-tight">
                         <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
                           {title}
                         </Link>
